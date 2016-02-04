1cb9c0256bd472c96a0d21665d7c51c7a64bfbc6
chris berry <chrisberry321@yahoo.com>
2016-02-04 15:43:39 -0800

    merge my work to master

--- js/scripts.js
+++ js/scripts.js
@@ -11,17 +11,31 @@ function Location(name, description){
 };
 
 function Player(){
-this.location = lobby;
-this.inventory = [];
+  this.location = lobby;
+  this.inventory = [];
 };
 
 function PuzzleMessage(message){
   this.message = message;
 };
 
-var guardAnswer = new PuzzleMessage("the guard looks at you with meagre respect in his eyes and slips an IDCARD into your wallet. 'Good job kid, you passed the first test.'")
+function commandMessage(message){
+  this.message = message;
+};
 
- var lobby = new Location('lobby', "You are in the lobby of the epicodus building. There is an elevator and a bored security guard playing solitaire on his computer. ENTER the elevator, TALK to the Security Guard, or OUTSIDE the building.");
+
+
+function item(){
+    for(var i=0; i < player.inventory.length ; i++)
+    if (player.inventory.indexOf("IdCard") >= 0){
+        return true;
+    }
+};
+
+
+var guardAnswer = new PuzzleMessage("the guard looks at you with meagre respect in his eyes and slips an IDCARD into your wallet. 'Good job kid, you passed the first test, now you can ENTER the elevator.'")
+
+ var lobby = new Location('lobby', "You are in the lobby of the epicodus building. There is an elevator and a bored security guard playing solitaire on his computer. ENTER the elevator, TALK to the Security Guard, or go OUTSIDE the building.");
 
  var securityGuard = new Location('security guard', "The security guard eyes you suspiciously. If you think you're so smart, what is the shortcut for clearing the console?");
 
@@ -31,45 +45,71 @@ var guardAnswer = new PuzzleMessage("the guard looks at you with meagre respect 
 
  var outside = new Location('outside', "Outside a homeless man starts yelling at you to take a shower. Humiliated, you slink home. Unfortunately you now have to start a NEW GAME");
 
+ var epicodus = new Location ('epicodus', 'A huge room full of epicodus students working hard on their various projects (SIT down at a computer, or CHOOSE a partner for the day.)');
+
+ var wrongfloor = new Location('wrongfloor', "You wander for hours around the huge building unable to find the epicodus office. After three days they find you in the bottom of the elevator shaft.")
+
+ var computer = new Location('computer', "The computer screen swims in front of you dizzily. You are much too tired to work right now.")
+
+ function addIdCard(input) {
+  //  debugger;
+   var inputCmd = document.getElementById("storyText");
+   if (input === "alt k") {
+     console.log("idCard");
+     player.inventory.push("idCard");
+     $("#inventory").append("IdCard");
+     inputCmd.value = guardAnswer.message;
+     $("input#commandBox").val("");
+   }
+   else {
+     inputCmd.value = "wrong answer kid";
+   }
+ }
+
   function enterCommand(){
-    var inputted = $("input#commandBox").val();
-    // var inputtedResponse = $("input#responseBox").val();
-    // var questionAnswer = $("form#modalInput").val();
+    // debugger;
+    var input = $("input#commandBox").val();
     var inputCmd = document.getElementById("storyText");
-    // alert(inputted);
-    if (inputted === "talk") {
+    // debugger;
+    if (input === "talk") {
       inputCmd.value = securityGuard.name + ": " + securityGuard.description;
       $("input#commandBox").val("");
-
-      $("#inputCommands").submit(function(event) {
-        event.preventDefault();
-        var inputted2 = $("input#commandBox").val();
-        if (inputted2 === "alt k") {
-          $("input#commandBox").val("");
-          console.log("idCard");
-          player.inventory.push("idCard");
-          inputCmd.value = guardAnswer.message;
-        }
-      });
-    }
-    if (inputted === "enter") {
-      //player.inventory.contain("idCard"
-      console.log("enter");
+    } else if (input === "enter" && player.inventory[0] === "idCard") {
       inputCmd.value = elevatorWithID.name + ": " + elevatorWithID.description;
-    }
-    if (inputted === "enter") {
+      return secondLevel()
+      // elevator === true than run function floor choice
+
+      $("input#commandBox").val("");
+    } else if (input === "enter") {
       inputCmd.value = elevator.name + ": " + elevator.description;
+      $("input#commandBox").val("");
+    } else if (input === "alt k") {
+      addIdCard(input);
+      $("input#commandBox").val("");
+    } else if (input === "outside"){
+        inputCmd.value = outside.name + ": " + outside.description;
+        $("input#commandBox").val("");
+    } else if (input === "new game") {
+          window.location.href = "index.html";
+          $("input#commandBox").val("");
+    } else{
+      console.log("fail");
     }
-    if (inputted === "new game") {
-      window.location.href = "index.html";
-    }
-    if (inputted === "outside"){
-      inputCmd.value = outside.name + ": " + outside.description;
-    }
-    // else {
-    //   inputCmd.value = "not a valid command."
-    // }
   }
+    function secondLevel() {
+      var input = $("input#commandBox").val();
+      var inputCmd = document.getElementById("storyText");
+    if (input === "floor 8"){
+        inputCmd.value = epicodus.name + ": " + epicodus.description;
+        $("input#commandBox").val("");
+      }
+
+      // else if (input !== "floor 8"){
+      //   inputCmd.value = wrongfloor.name + ": " + wrongfloor.description;
+      // }
+    }
+
+
   //clear commandBox
   //user enters response
   //gather the users response and save var
@@ -81,62 +121,11 @@ $(document).ready(function(){
   $("#inputCommands").submit(function(event) {
     event.preventDefault();
     enterCommand();
-  });
-
-    //clear out the command box
-    document.getElementById("commandBox").value = "";
-
-
-  $("form#answerSubmit").submit(function(event) {
-
-    debugger;
-    var inputted = $("input#commandBox").val();
-    // var inputtedResponse = $("input")
-    // var questionAnswer = $("input#modalInput").val();
-    console.log("test");
-    event.preventDefault();
-    enterCommand();
+    secondLevel();
   });
 });
 
-// function updateInventoryDisplay(newItem, command){
-//   if(command === "take" && newItem === "id"){
-//     $("p #inventory").append("<span p id="'newItem'" class='item'>"+newItem+"</span>");
-//   }
-// }
-
-// // his code_>>
-//
-//             //taken and modified from StackOverflow.com
-//             //this loop search through the array inventory looking for the value of "item". When found, it deletes the item.
-//             for(var i in inventory){
-//                 if(inventory[i] === item){
-//                 array.splice(i,1);
-//                 break;
-//                 }
-//             }
-//             break;
-//
-//         default:
-//             setMsg = "Error encountered. Please try again.";
-//             updateDisplay(setMsg);
-//             break;
-//     }
-//
-// }
-//
-// //updates #invBox div with new item
-// function updateInventoryDisplay(newItem, action){
-//
-//     if(action === "add" && newItem === "map"){
-//         $("#invBox").append("<span id='"+newItem+"' class='item' onClick='popupMap()'>"+newItem+"</span>");
-//     }
-//     else if(action === "add"){
-//          $("#invBox").append("<span id='"+newItem+"' class='item'>"+newItem+"</span>");
-//     }
-//     else{ //delete the item
-//         $("#"+newItem+"").remove();
-//     }
-//
-//
-// }
+// var firstChoice =  enterCommand();
+//   var secondChoice  = floorChoice(firstChoice);
+//   var thirdChoice = thirdFloor(seconChoice);
+//   var finalChoice =
