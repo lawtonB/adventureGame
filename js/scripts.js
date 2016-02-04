7979b8a61662a9e74e967286c88ae45db7a53f55
lawton browning <lawton.a.browning@gmail.com>
2016-02-03 16:34:09 -0800

    add backend logic, tie input box to logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,103 @@
+var player = new Player();
+
 function init(){
   var txtBox = document.getElementById("storyText");
-  txtBox.value = "You are in the lobby of the epicodus building. There is an elevator and a bored security guard playing solitaire on his computer.  (ENTER the elevator or TALK to the Security Guard.)";
-}
-var inventory = [];
-var idCard = inventory[0]
-
-function hasId(item){
-  if(inventory[0] === "IDCARD"){
-    return true;
-  }
-  else{
-    return false;
-  }
+  txtBox.value = lobby.name + ": " + lobby.description;
 }
 
+function Location(name, description){
+  this.name = name;
+  this.description = description;
+};
 
-//
+function Player(){
+this.location = lobby;
+this.inventory = [];
+};
 
-// function gameOver(){
-//   var newGame = init();
-//   if (command === newGame) {
-//     alert("new game");
-//     window.location.href = "index.html";
-//   }
-// }
+function PuzzleMessage(message){
+  this.message = message;
+};
+
+var guardAnswer = new PuzzleMessage("the guard looks at you with meagre respect in his eyes and slips an IDCARD into your wallet. 'Good job kid, you passed the first test.'")
+
+ var lobby = new Location('lobby', "You are in the lobby of the epicodus building. There is an elevator and a bored security guard playing solitaire on his computer. ENTER the elevator, TALK to the Security Guard, or OUTSIDE the building.");
+
+ var securityGuard = new Location('security guard', "The security guard eyes you suspiciously. If you think you're so smart, what is the shortcut for clearing the console?");
+
+ var elevator = new Location('elevator', "The elevator hums ominously at you. It is obviously waiting for something.");
+
+ var elevatorWithID = new Location('elevator', "You wait around for a moment. DING! Your keycard flashes and the elevator starts to move. Which FLOOR?");
+
+ var outside = new Location('outside', "Outside a homeless man starts yelling at you to take a shower. Humiliated, you slink home. Unfortunately you now have to start a NEW GAME");
+
+  function enterCommand(){
+    var inputted = $("input#commandBox").val();
+    // var inputtedResponse = $("input#responseBox").val();
+    // var questionAnswer = $("form#modalInput").val();
+    var inputCmd = document.getElementById("storyText");
+    // alert(inputted);
+    if (inputted === "talk") {
+      inputCmd.value = securityGuard.name + ": " + securityGuard.description;
+      $("input#commandBox").val("");
+
+      $("#inputCommands").submit(function(event) {
+        event.preventDefault();
+        var inputted2 = $("input#commandBox").val();
+        if (inputted2 === "alt k") {
+          $("input#commandBox").val("");
+          console.log("idCard");
+          player.inventory.push("idCard");
+          inputCmd.value = guardAnswer.message;
+        }
+      });
+    }
+    if (inputted === "enter") {
+      //player.inventory.contain("idCard"
+      console.log("enter");
+      inputCmd.value = elevatorWithID.name + ": " + elevatorWithID.description;
+    }
+    if (inputted === "enter") {
+      inputCmd.value = elevator.name + ": " + elevator.description;
+    }
+    if (inputted === "new game") {
+      window.location.href = "index.html";
+    }
+    if (inputted === "outside"){
+      inputCmd.value = outside.name + ": " + outside.description;
+    }
+    // else {
+    //   inputCmd.value = "not a valid command."
+    // }
+  }
+  //clear commandBox
+  //user enters response
+  //gather the users response and save var
+  //if else statement to test resposne
+
+$(document).ready(function(){
+  init();
+
+  $("#inputCommands").submit(function(event) {
+    event.preventDefault();
+    enterCommand();
+  });
+
+    //clear out the command box
+    document.getElementById("commandBox").value = "";
 
 
+  $("form#answerSubmit").submit(function(event) {
 
-
+    debugger;
+    var inputted = $("input#commandBox").val();
+    // var inputtedResponse = $("input")
+    // var questionAnswer = $("input#modalInput").val();
+    console.log("test");
+    event.preventDefault();
+    enterCommand();
+  });
+});
 
 // function updateInventoryDisplay(newItem, command){
 //   if(command === "take" && newItem === "id"){
@@ -70,78 +140,3 @@ function hasId(item){
 //
 //
 // }
-
-function updateInventory(command){
-  if (command === "take") {
-    inventory.push("ID CARD");
-  } else {
-  }
-  console.log(inventory);
-}
-
-// function enterAnswer(){
-//   var inputted = $("input#commandBox").val();
-//   var answerCmd = document.getElementById("storyText");
-// }
-  function enterCommand(){
-    var inputted = $("input#commandBox").val();
-    var inputCmd = document.getElementById("storyText");
-    // alert(inputted);
-    if (inputted === "talk") {
-      inputCmd.value = "The security guard eyes you suspiciously. If you think you're so smart, what is the shortcut for clearing the console?"
-    }
-    else if (inputted === "alt k") {
-      inputCmd.value = "Here You go *hands id card*" && updateInventory();
-      console.log(inputted);
-    }
-
-    else if (inputted === "enter" && inventory["IDCARD"]) {
-      inputCmd.value = "The elevator hums silently as you fidgit trying to act like you belong. Ding! the doors open and you walk into the elevator.(press UP or DOWN depending on which direction you would like to go.";
-    }
-
-    else if (inputted === "enter") {
-      inputCmd.value = "The elevator hums ominously at you. It is obviously waiting for something.";
-    }
-
-    else if (inputted === "take") {
-      inputCmd.value = "You are now holding a dull plastic epicodus keycard in your hand. Now you can ENTER the elevator, and it will work.";
-    }
-    else if (inputted === "newgame") {
-      inputCmd.value = "this is the wrong way to do this"
-      window.location.href = "index.html";
-
-    }
-    else {
-      inputCmd.value = "not a valid command."
-    }
-  }
-
-$(document).ready(function(){
-  init();
-
-  var currentLocation = "Home";
-
-
-  function updateLocation(location){
-      currentLocation = location;
-  }
-
-  function updateDisplay(message){
-      var msg = message;
-      var txtBox = document.getElementById("storyText");
-      txtBox.value = msg + "\n\n" + txtBox.value;
-  }
-
-  function listCommands(){
-    var setMsg = "valid commands are: 'enter' 'talk' ";
-    updateDisplay(setMsg);
-  }
-
-  $("#inputCommands").submit(function(event) {
-    event.preventDefault();
-    enterCommand();
-  });
-
-    //clear out the command box
-    document.getElementById("commandBox").value = "";
-})
